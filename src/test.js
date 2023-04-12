9abac261ed6a102726279d13fbcaefcd8a82f5ba
majdaburas5 <aburasm54@gmail.com>
2023-04-13 01:06:02 +0300

    add marbles array

--- src/test.js
+++ src/test.js
@@ -1,8 +1,9 @@
-import marbles from "../server/database/databse";
+// import marbles from "./ ";
+// const Marble = require("../server/models/marbleModel");
 
-console.log(marbles);
+// console.log(marbles);
 // marbles.map((m) => {
-//   let m1 = {
+//   let m1 = new Marble({
 //     code: m.code,
 //     type: m.type,
 //     price: m.price,
@@ -11,6 +12,6 @@ console.log(marbles);
 //     name: m.name,
 //     img: m.img,
 //     color: m.color,
-//   };
+//   });
 //   m1.save();
 // });
