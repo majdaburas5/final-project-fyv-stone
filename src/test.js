f714138c9068854d9e1ebd9a8134df65662f98fb
Majd-Turk <majd_turk@hotmail.com>
2023-04-13 12:21:52 +0300

    delete test.js

--- src/test.js
+++ src/test.js
@@ -1,16 +1,17 @@
 import marbles from "../server/database/databse";
+const Marble = require("../server/models/marbleModel");
 
 console.log(marbles);
-// marbles.map((m) => {
-//   let m1 = {
-//     code: m.code,
-//     type: m.type,
-//     price: m.price,
-//     quantity: m.quantity,
-//     style: m.style,
-//     name: m.name,
-//     img: m.img,
-//     color: m.color,
-//   };
-//   m1.save();
-// });
+marbles.map((m) => {
+  let m1 = new Marble({
+    code: m.code,
+    type: m.type,
+    price: m.price,
+    quantity: m.quantity,
+    style: m.style,
+    name: m.name,
+    img: m.img,
+    color: m.color,
+  });
+  m1.save();
+});
