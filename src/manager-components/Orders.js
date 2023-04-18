110ea54d796a93478f6c785499ba347b0ba73bda
majdaburas5 <aburasm54@gmail.com>
2023-04-19 00:16:32 +0300

    finish orders view

--- src/manager-components/Orders.js
+++ src/manager-components/Orders.js
@@ -4,49 +4,46 @@ import "../manager-css/Orders.css";
 
 export default function Orders() {
   const [orders, setOrders] = useState([]);
+  const [customerNames, setCustomerNames] = useState([]);
+  const [customerPhones, setCustomerPhones] = useState([]);
 
   useEffect(() => {
     marblesAddedToCart().then((res) => {
       setOrders(res);
+      const promises = res.map((order) => getCustomer(order.customerId));
+      Promise.all(promises).then((customers) => {
+        const names = customers.map((customer) => customer[0].name);
+        const phones = customers.map((customer) => customer[0].phone);
+        setCustomerNames(names);
+        setCustomerPhones(phones);
+      });
     });
   }, []);
-  console.log(orders);
-
-  const getCustomerName = async function (id) {
-    await getCustomer(id).then((res) => {
-      console.log(res[0].name);
-      return res[0].name;
-    });
-  };
-
-  const getCustomerPhone = async function (id) {
-    await getCustomer(id).then((res) => {
-      console.log(res[0].phone);
-      return res[0].phone;
-    });
-  };
 
   return (
     <div>
       <h1 className="order-title">Orders</h1>
       <table className="order-table">
-        <tr>
-          <th>Order Number</th>
-          <th>Date</th>
-          <th>Customer Name</th>
-          <th>Phone</th>
-          <th>Status</th>
-        </tr>
-        {orders &&
-          orders.map((order) => (
+        <thead>
+          <tr>
+            <th>Order Number</th>
+            <th>Date</th>
+            <th>Customer Name</th>
+            <th>Phone</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order, index) => (
             <tr key={order._id}>
               <td>{order.orderNumber}</td>
               <td>{order.orderDate}</td>
-              <td>{() => getCustomerName(order.customerId)}</td>
-              <td>{() => getCustomerPhone(order.customerId)}</td>
+              <td>{customerNames[index]}</td>
+              <td>{customerPhones[index]}</td>
               <td>{order.status}</td>
             </tr>
           ))}
+        </tbody>
       </table>
     </div>
   );
