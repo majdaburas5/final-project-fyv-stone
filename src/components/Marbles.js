0695585d62f09ab4e022bbc3aaeb564f6ea4bfb7
Rania Haj <haj.rania@hotmail.com>
2023-04-25 14:45:12 +0300

    clear buttun

--- src/components/Marbles.js
+++ src/components/Marbles.js
@@ -11,6 +11,7 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { marblesFromDB, filteredMarbles } from "../api";
 import FilterButton from "./FilterButton";
+import { Button } from "react-bootstrap";
 
 export default function Marbles({ updateCartArray, cartArray, isLoggedIn }) {
   const [marbles, setMarbles] = useState([]);
