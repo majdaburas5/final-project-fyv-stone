064e1a9ba6b454894911491746b2f503ef6e6bf9
tamem waked <tamem054@gmail.com>
2023-04-30 17:35:04 +0300

    manager style fixed

--- src/components/Marbles.js
+++ src/components/Marbles.js
@@ -148,9 +148,7 @@ export default function Marbles({ updateCartArray, cartArray, isLoggedIn }) {
                         Add To Cart
                       </button>
                     </>
-                  ) : (
-                    <div> </div>
-                  )}
+                  ) : null}
                 </div>
               </div>
             );
